7a2f805ce72ec19b1fb6ebb6b1642ed7bf2edcf2
Cássio Paixão <cassiopaixaoti@gmail.com>
2019-06-30 11:10:04 -0300

    Adjust http errors

--- src/interfaces/http/errors/HttpErrors.js
+++ src/interfaces/http/errors/HttpErrors.js
@@ -5,16 +5,88 @@ const {
 } = require('src/interfaces/http/constants/HttpConstants');
 
 const HttpErrors = class extends ApplicationException {
-  static badRequest(errors, message = HttpMessage.BAD_REQUEST) {
+  static badRequest(
+    errors,
+    message = HttpMessage.BAD_REQUEST,
+    errorCode = '',
+    statusCode = HttpCode.BAD_REQUEST
+  ) {
     return new HttpErrors(
       Object.assign({ message, errors }),
-      HttpCode.BAD_REQUEST,
+      statusCode,
+      errorCode ? errorCode : statusCode,
       true
     );
   }
 
-  static notFound(errors, message = HttpMessage.NOT_FOUND) {
-    return new HttpErrors({ message, errors }, HttpCode.NOT_FOUND, true);
+  static forbidden(
+    errors,
+    message = HttpMessage.FORBIDDEN,
+    errorCode = '',
+    statusCode = HttpCode.FORBIDDEN
+  ) {
+    return new HttpErrors(
+      Object.assign({ message, errors }),
+      statusCode,
+      errorCode ? errorCode : statusCode,
+      true
+    );
+  }
+
+  static notFound(
+    errors,
+    message = HttpMessage.NOT_FOUND,
+    errorCode = '',
+    statusCode = HttpCode.NOT_FOUND
+  ) {
+    return new HttpErrors(
+      Object.assign({ message, errors }),
+      statusCode,
+      errorCode ? errorCode : statusCode,
+      true
+    );
+  }
+
+  static internalServer(
+    errors,
+    message = HttpMessage.INTERNAL_SERVER_ERROR,
+    errorCode = '',
+    statusCode = HttpCode.INTERNAL_SERVER_ERROR
+  ) {
+    return new HttpErrors(
+      Object.assign({ message, errors }),
+      statusCode,
+      errorCode ? errorCode : statusCode,
+      true
+    );
+  }
+
+  static unprocessable(
+    message = HttpMessage.UNPROCESSABLE_ENTITY,
+    errorCode = '',
+    statusCode = HttpCode.UNPROCESSABLE_ENTITY,
+    errors
+  ) {
+    return new HttpErrors(
+      Object.assign({ message, errors }),
+      statusCode,
+      errorCode ? errorCode : statusCode,
+      true
+    );
+  }
+
+  static serviceUnavailable(
+    message = HttpMessage.SERVICE_UNAVAILABLE,
+    errorCode = '',
+    statusCode = HttpCode.SERVICE_UNAVAILABLE,
+    errors
+  ) {
+    return new HttpErrors(
+      Object.assign({ message, errors }),
+      statusCode,
+      errorCode ? errorCode : statusCode,
+      true
+    );
   }
 };
 
